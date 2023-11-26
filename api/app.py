e363ca6a97a39c212df2863079c8a99e8750c312
csznbacks@gmail.com <csznbacks@gmail.com>
2023-11-27 03:59:37 +0800

    update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, g, flash, abort, jsonify, send_file, make_response
+from flask import Flask, render_template, request, redirect, url_for, g, flash, abort, jsonify, send_file, Response, make_response
 import requests
 import base64
 import hashlib
@@ -13,13 +13,13 @@ import re
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Hikari"
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home():
   file = "Hikari.lua"
   
   return send_file(file, mimetype="text/plain")
   
-@app.route('/components/library')
+@app.route('/components/library', methods=["GET"])
 def library():
   file = "Hikari[Components]/Library.lua"
   
