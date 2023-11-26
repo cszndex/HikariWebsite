5b590b2374a3a8d9720e54e8052fba0c978c3b68
csznbacks@gmail.com <csznbacks@gmail.com>
2023-11-27 04:08:03 +0800

    update app.py

--- api/app.py
+++ api/app.py
@@ -21,7 +21,7 @@ def home():
   
 @app.route('/components/library', methods=["GET"])
 def library():
-  filePath = "/Components/Library.lua"
+  filePath = "Components/Library.lua"
   
   file = open(filePath, 'r')
   
